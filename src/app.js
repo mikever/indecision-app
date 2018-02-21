ce26d81986c99c120888fc04c6e465a76e0fb909
Michael <mveronie@gmail.com>
2018-02-21 06:59:20 -0500

    Set up default props

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ class IndecisionApp extends React.Component {
     this.handleAddOption = this.handleAddOption.bind(this);
 
     this.state = {
-      options: []
+      options: props.options
     }
   }
 
@@ -41,12 +41,11 @@ class IndecisionApp extends React.Component {
   }
 
   render() {
-    const title = `Indecision`;
     const subtitle = `Put your life in the hands of a computer`;
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action 
           hasOptions={this.state.options.length > 0} 
           handlePick={this.handlePick}
@@ -63,13 +62,21 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+  options: []
+};
+
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   );
+};
+
+Header.defaultProps = {
+  title: `Indecision`
 };
 
 const Action = (props) => {
@@ -136,14 +143,5 @@ class AddOption extends React.Component {
     );
   }
 }
-
-// const User = (props) => {
-//   return (
-//     <div>
-//       <p>Name: {props.name}</p>
-//       <p>Age: {props.age}</p>
-//     </div>
-//   );
-// };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById(`app`));
