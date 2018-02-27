5b82d1da93b17e800dd263143b6207da3bff57ae
Michael <mveronie@gmail.com>
2018-02-27 02:47:27 -0500

    created imports/exports from component files.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
-console.log(`app.js is running`);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+
+ReactDOM.render(<IndecisionApp />, document.getElementById(`app`));
 
