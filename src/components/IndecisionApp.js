82f9cfba44204a81d3815c4ee8d0ac4cfbaeb902
Michael <mveronie@gmail.com>
2018-03-01 03:15:41 -0500

    Add modal box to app.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
-      options: []
+      options: [],
+      selectedOption: undefined
     };
 
   handleDeleteOptions = () => {
@@ -22,7 +24,9 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState(() => ({
+      selectedOption: option
+    }));
   }
 
   handleAddOption = (option) => {
@@ -33,6 +37,10 @@ export default class IndecisionApp extends React.Component {
     }
 
     this.setState((prevState) => ({ options: prevState.options.concat(option) }));
+  }
+
+  handleClearSelectedOption = () => {
+    this.setState(() => ({ selectedOption: undefined }));
   }
 
   componentDidMount() {
@@ -76,6 +84,10 @@ export default class IndecisionApp extends React.Component {
         />
         <AddOption
           handleAddOption={this.handleAddOption}
+        />
+        <OptionModal 
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
       </div>
     );
